e484a21e35d3af3f5ba675e9113e473ca2d95b84
markrybaltovskij <markrybaltovskij@gmail.com>
2025-03-11 16:39:29 +0300

    Initial commit

---
+++ src/main/java/org/example/Game.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Game {
+    public static void main(String[] args) {
+        String secretWord = WordSelect.getWord();
+        System.out.println(secretWord);
+        
+    }
+}
---
+++ src/main/java/org/example/WordSelect.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.regex.Pattern;
+
+public class WordSelect {
+
+    private static final String FILE_PATH = "src/main/java/org/example/WordsList.txt";
+
+    public static String getWord() {
+        List<String> wordsList = readWordsFromFile();
+        return randomWord(wordsList);
+    }
+
+    private static List<String> readWordsFromFile() {
+        List<String> words = new ArrayList<>();
+        Pattern pattern = Pattern.compile("[^а-яА-Яa-zA-Z]+");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(WordSelect.FILE_PATH, java.nio.charset.StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] wordArray = pattern.split(line);
+                for (String word : wordArray) {
+                    if (!word.isEmpty()) {
+                        words.add(word);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при чтении файла: " + e.getMessage());
+        }
+
+        return words;
+    }
+
+    private static String randomWord(List<String> words) {
+        Random random = new Random();
+        return words.get(random.nextInt(words.size()));
+    }
+}
